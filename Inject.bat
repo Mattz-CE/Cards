15e3772d68c3e8b809ad857c40c3fdc19debe21c
Matthew Zhou <61751438+Mattz-CE@users.noreply.github.com>
2020-12-16 13:57:00 +0800

    UPDATE

--- Inject.bat
+++ Inject.bat
@@ -1,12 +1,14 @@
 REM Pictures and Almost everything else
-xcopy KAN "C:\Users\matth\AppData\Local\Forge\Cache\pics\cards\KAN" /I /Y
-xcopy D:\m\File\Cards\Aimport\res "D:\m\Games\Forge 36\res" /I /Y /S
+cd C:\Users\mattz\Documents\GitHub\Cards
+
+xcopy KAN "%localappdata%\Forge\Cache\pics\cards\KAN" /I /Y
+xcopy "C:\Users\mattz\Documents\GitHub\Cards\Aimport\res" "C:\Users\mattz\Documents\Softwares\Forge 36\res" /I /Y /S
 
 REM Compile Cards
-cd "D:\m\Games\Forge 36\res\cardsfolder"
-start cmd.exe /C "D:\m\Games\Forge 36\res\cardsfolder\ZIP.bat"
+cd "C:\Users\mattz\Documents\Softwares\Forge 36\res\cardsfolder"
+ start cmd.exe /C "C:\Users\mattz\Documents\Softwares\Forge 36\res\cardsfolder\ZIP.bat"
 
 REM Token Pictures
-xcopy "D:\m\File\Cards\Aimport\res\tokens" "C:\Users\matth\AppData\Local\Forge\Cache\pics\tokens" /I /Y /S
+xcopy "C:\Users\mattz\Documents\GitHub\Cards\Aimport\res\tokens" "%localappdata%\Forge\Cache\pics\tokens" /I /Y /S
 
 pause
